857abbece690e6eff1abc011286592049c79ee40
DiegoMurcia2022 <diegomurciac2220@gmail.com>
2021-04-05 05:07:22 -0500

    spring-boot funcional

---
+++ src/main/java/edu/eci/arsw/ecibastas/ECIBastasApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.ecibastas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECIBastasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECIBastasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/eci/arsw/ecibastas/ECIBastasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.ecibastas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECIBastasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
