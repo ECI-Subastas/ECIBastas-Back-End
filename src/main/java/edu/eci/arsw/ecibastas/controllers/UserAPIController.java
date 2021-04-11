2142be820f21ae776f6aca84bd4c8023dc4ddaf6
DiegoMurcia2220 <diegomurciac2220@gmail.com>
2021-04-11 04:06:39 -0500

    RestController para usuarios v1.1

--- src/main/java/edu/eci/arsw/ecibastas/controllers/UserAPIController.java
+++ src/main/java/edu/eci/arsw/ecibastas/controllers/UserAPIController.java
@@ -3,6 +3,7 @@ package edu.eci.arsw.ecibastas.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,12 +18,13 @@ import edu.eci.arsw.ecibastas.services.exceptions.UserServiceException;
 
 @RestController
 @CrossOrigin(origins = "*")
+@Controller("/user")
 public class UserAPIController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(path = "/user/createNewUser", method = RequestMethod.POST)
-    public ResponseEntity<?> createNewUserEntity(@RequestBody User user) {
+    @RequestMapping(value = "/createNewUser", method = RequestMethod.POST)
+    public ResponseEntity<?> createNewUser(@RequestBody User user) {
         try {
             userService.createNewUser(user);
 
@@ -32,8 +34,8 @@ public class UserAPIController {
         }
     }
 
-    @RequestMapping(path = "/user/changeRole", method = RequestMethod.POST)
-    public ResponseEntity<?> changeUserRoleEntity(@PathVariable(name = "nickname") String nickname, @PathVariable(name = "email") String email, @PathVariable(name = "role") String role) {
+    @RequestMapping(value = "/changeRole", method = RequestMethod.POST)
+    public ResponseEntity<?> changeUserRole(@PathVariable(name = "nickname") String nickname, @PathVariable(name = "email") String email, @PathVariable(name = "role") String role) {
         try {
             userService.changeUserRole(nickname, email, role);
 
@@ -43,8 +45,8 @@ public class UserAPIController {
         }
     }
 
-    @RequestMapping(path = "/user", method = RequestMethod.GET)
-    public ResponseEntity<?> getUserByNicknameEntity(@RequestParam(name = "nickname") String nickname) {
+    @RequestMapping(value = "/nickname", method = RequestMethod.GET)
+    public ResponseEntity<?> getUserByNickname(@RequestParam(name = "value") String nickname) {
         try {
             return new ResponseEntity<>(userService.getUserByNickname(nickname), HttpStatus.FOUND);
         } catch (UserServiceException e) {
@@ -52,8 +54,8 @@ public class UserAPIController {
         }
     }
 
-    @RequestMapping(path = "/user", method = RequestMethod.GET)
-    public ResponseEntity<?> getUserByEmailEntity(@RequestParam(name = "email") String email) {
+    @RequestMapping(value = "/email", method = RequestMethod.GET)
+    public ResponseEntity<?> getUserByEmail(@RequestParam(name = "value") String email) {
         try {
             return new ResponseEntity<>(userService.getUserByEmail(email), HttpStatus.FOUND);
         } catch (UserServiceException e) {
